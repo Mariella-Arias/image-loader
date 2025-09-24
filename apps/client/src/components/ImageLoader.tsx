8cef05282ff40fecc194dc4fec2e54cee0b22ec7
mariella-arias <arias.mariella1@gmail.com>
2025-09-23 21:23:17 -0500

    Add empty images component

--- apps/client/src/components/ImageLoader.tsx
+++ apps/client/src/components/ImageLoader.tsx
@@ -133,130 +133,143 @@ const ImageLoader = ({ onSubmit, onDelete, photos }: ImageGridProps) => {
           overflow: 'auto',
         }}
       >
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          sx={{ padding: '1.5rem', overflow: 'auto' }}
-        >
-          {photos.map(({ path, label }) => (
-            <Grid
-              key={label}
-              sx={{
-                borderRadius: '10px',
-                width: '250px',
-                height: '270px',
-                position: 'relative',
-              }}
-            >
-              <Modal
-                open={modalOpen}
-                onClose={handleClose}
-                aria-labelledby="modal-confirm-title"
-                aria-describedby="modal-confirm-confirmation"
+        {photos.length > 0 ? (
+          <Grid
+            container
+            spacing={3}
+            direction="row"
+            sx={{ padding: '1.5rem', overflow: 'auto' }}
+          >
+            {photos.map(({ path, label }) => (
+              <Grid
+                key={label}
+                sx={{
+                  borderRadius: '10px',
+                  width: '250px',
+                  height: '270px',
+                  position: 'relative',
+                }}
               >
+                <Modal
+                  open={modalOpen}
+                  onClose={handleClose}
+                  aria-labelledby="modal-confirm-title"
+                  aria-describedby="modal-confirm-confirmation"
+                >
+                  <Box
+                    sx={{
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      width: 400,
+                      bgcolor: 'background.paper',
+                      borderRadius: '10px',
+                      boxShadow: 24,
+                      p: 4,
+                    }}
+                  >
+                    <Typography
+                      id="modal-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      Delete Image
+                    </Typography>
+                    <Typography
+                      id="modal-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      This action is irreversible. Are you sure you want to
+                      delete this image?
+                    </Typography>
+                    <Box sx={{ mt: 2, display: 'flex', gap: '1rem' }}>
+                      <Button
+                        variant="contained"
+                        color="error"
+                        sx={{ flex: 1 }}
+                        onClick={() => {
+                          // delete image
+                          if (targetFile) {
+                            onDelete(targetFile);
+                          }
+
+                          // hide modal
+                          handleClose();
+                        }}
+                      >
+                        Delete
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        sx={{ flex: 1 }}
+                        onClick={() => handleClose()}
+                      >
+                        Cancel
+                      </Button>
+                    </Box>
+                  </Box>
+                </Modal>
+
                 <Box
                   sx={{
+                    height: '100%',
+                    width: '100%',
                     position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
-                    bgcolor: 'background.paper',
-                    borderRadius: '10px',
-                    boxShadow: 24,
-                    p: 4,
+                    overflow: 'hidden',
                   }}
+                  onClick={() => setTargetFile(path)}
                 >
-                  <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
+                  <IconButton
+                    onClick={handleOpen}
+                    sx={{
+                      position: 'absolute',
+                      zIndex: '30',
+                      right: 0,
+                      transform: 'translate(-0.25rem, 0.25rem)',
+                    }}
                   >
-                    Delete Image
-                  </Typography>
-                  <Typography
-                    id="modal-modal-description"
-                    sx={{ mt: 2 }}
+                    <CloseIcon />
+                  </IconButton>
+                  <img
+                    src={`http://localhost:3000/${path}`}
+                    alt="image-alt"
+                    style={{
+                      position: 'absolute',
+                      height: 'auto',
+                      width: '250px',
+                      aspectRatio: 1,
+                      borderRadius: '10px',
+                    }}
+                  />
+                  <figcaption
+                    style={{
+                      position: 'absolute',
+                      bottom: 0,
+                      paddingLeft: '0.5rem',
+                      transform: 'translateY(0.25rem)',
+                      fontStyle: 'italic',
+                    }}
                   >
-                    This action is irreversible. Are you sure you want to delete
-                    this image?
-                  </Typography>
-                  <Box sx={{ mt: 2, display: 'flex', gap: '1rem' }}>
-                    <Button
-                      variant="contained"
-                      color="error"
-                      sx={{ flex: 1 }}
-                      onClick={() => {
-                        // delete image
-                        if (targetFile) {
-                          onDelete(targetFile);
-                        }
-
-                        // hide modal
-                        handleClose();
-                      }}
-                    >
-                      Delete
-                    </Button>
-                    <Button
-                      variant="outlined"
-                      color="error"
-                      sx={{ flex: 1 }}
-                      onClick={() => handleClose()}
-                    >
-                      Cancel
-                    </Button>
-                  </Box>
+                    {label}
+                  </figcaption>
                 </Box>
-              </Modal>
-
-              <Box
-                sx={{
-                  height: '100%',
-                  width: '100%',
-                  position: 'absolute',
-                  overflow: 'hidden',
-                }}
-                onClick={() => setTargetFile(path)}
-              >
-                <IconButton
-                  onClick={handleOpen}
-                  sx={{
-                    position: 'absolute',
-                    zIndex: '30',
-                    right: 0,
-                    transform: 'translate(-0.25rem, 0.25rem)',
-                  }}
-                >
-                  <CloseIcon />
-                </IconButton>
-                <img
-                  src={`http://localhost:3000/${path}`}
-                  alt="image-alt"
-                  style={{
-                    position: 'absolute',
-                    height: 'auto',
-                    width: '250px',
-                    aspectRatio: 1,
-                    borderRadius: '10px',
-                  }}
-                />
-                <figcaption
-                  style={{
-                    position: 'absolute',
-                    bottom: 0,
-                    paddingLeft: '0.5rem',
-                    transform: 'translateY(0.25rem)',
-                    fontStyle: 'italic',
-                  }}
-                >
-                  {label}
-                </figcaption>
-              </Box>
-            </Grid>
-          ))}
-        </Grid>
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Typography
+            variant="body1"
+            sx={{
+              padding: '1rem',
+              width: 'fit-content',
+              fontStyle: 'italic',
+            }}
+          >
+            No images in store
+          </Typography>
+        )}
       </Paper>
     </Box>
   );
